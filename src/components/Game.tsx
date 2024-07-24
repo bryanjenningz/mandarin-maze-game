f57b6c78518cb2c12e6cda24b3ef7bcf7f2db1db
Bryan Jennings <bryanjenningz@gmail.com>
2024-07-23 19:20:50 -0700

    Add a gray square

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,3 +1,7 @@
 export const Game = () => {
-  return <div className="text-center">2D Maze Game</div>;
+  return (
+    <div className="h-[100svh] flex justify-center items-center">
+      <div className="aspect-square bg-slate-800 w-full max-w-2xl"></div>
+    </div>
+  );
 };
