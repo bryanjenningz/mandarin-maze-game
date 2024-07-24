b8e6d881c459abe0b44e58cff2149d1c92b3f0ff
Bryan Jennings <bryanjenningz@gmail.com>
2024-07-23 19:49:12 -0700

    Display the game map

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,58 @@
+const gameMap = [
+  "#EEEE###############",
+  "#    #             #",
+  "#    #             #",
+  "#    #             #",
+  "#    #    ####     #",
+  "#                  #",
+  "#             #    #",
+  "######        #    #",
+  "#                  #",
+  "#       ####       #",
+  "#                  #",
+  "#                  #",
+  "#            #     #",
+  "######       #     #",
+  "#    #       #     #",
+  "#    #       #     #",
+  "#    #             #",
+  "#                  #",
+  "#                  #",
+  "####################",
+].map((row) => row.split(""));
+
 export const Game = () => {
   return (
     <div className="h-[100svh] flex justify-center items-center">
-      <div className="aspect-square bg-slate-800 w-full max-w-2xl"></div>
+      <div className="aspect-square bg-slate-800 w-full max-w-2xl flex flex-col">
+        {gameMap.map((row, y) => {
+          return (
+            <div key={y} className="flex h-[5%]">
+              {row.map((box, x) => {
+                return (
+                  <div
+                    key={x}
+                    className={
+                      "w-full " +
+                      ((): string => {
+                        switch (box) {
+                          case "#":
+                            return "bg-blue-800";
+                          case " ":
+                          case "E":
+                            return "bg-black";
+                          default:
+                            return "";
+                        }
+                      })()
+                    }
+                  ></div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
