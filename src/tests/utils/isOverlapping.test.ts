af325e19b7835b154cdbc3f1788311096d07af85
Bryan Jennings <bryanjenningz@gmail.com>
2024-07-28 15:44:48 -0700

    Add isOverlapping tests

---
+++ src/tests/utils/isOverlapping.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from "vitest";
+import { isOverlapping } from "../../utils/isOverlapping";
+
+describe("isOverlapping", () => {
+  const expectations: [
+    x: number,
+    y: number,
+    size: number,
+    expected: boolean
+  ][] = [
+    [0, 0, 20, false],
+    [0, 1, 20, false],
+    [1, 1, 20, true],
+    [0, 0, 21, true],
+    [20, 20, 20, true],
+    [40, 20, 20, false],
+    [39, 20, 20, true],
+    [39, 39, 20, true],
+    [39, 39, 1, true],
+    [39, 1, 20, true],
+    [1, 39, 20, true],
+    [1, 40, 20, false],
+    [40, 1, 20, false],
+  ];
+  test.each(expectations)(
+    "isOverlapping({ x: %i, y: %i, size: %i }, { x: 20, y: 20, size: 20 }) -> %j",
+    (x, y, size, expected) => {
+      expect(isOverlapping({ x, y, size }, { x: 20, y: 20, size: 20 })).toEqual(
+        expected
+      );
+    }
+  );
+});
