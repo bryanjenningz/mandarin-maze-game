90d6480330211661f1270b5496c99bafd167491f
Bryan Jennings <bryanjenningz@gmail.com>
2024-07-27 21:27:00 -0700

    Add test for monster health lowering

--- src/tests/utils/updateMonsters.test.ts
+++ src/tests/utils/updateMonsters.test.ts
@@ -4,10 +4,13 @@ import type { Bullet, Monster, MonsterRandomness } from "../../components/Game";
 
 describe("updateMonsters", () => {
   test("lowers monster health if bullets overlap", () => {
-    const monsters: Monster[] = [];
+    const monsters: Monster[] = [{ x: 10, y: 10, target: null, health: 100 }];
     const monsterRandomness: MonsterRandomness[] = [];
-    const bullets: Bullet[] = [];
+    const bullets: Bullet[] = [{ x: 10, y: 10, dx: 2, dy: 0 }];
     const newMonsters = updateMonsters(monsters, monsterRandomness, bullets);
-    expect(newMonsters).toEqual([]);
+    const expectedNewMonsters: Monster[] = [
+      { x: 10, y: 10, target: null, health: 90 },
+    ];
+    expect(newMonsters).toEqual(expectedNewMonsters);
   });
 });
