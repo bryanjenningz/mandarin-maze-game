31ae8e7c852889d1fc1f1645c63315f9ffa00938
Bryan Jennings <bryanjenningz@gmail.com>
2024-07-29 19:42:59 -0700

    Add tests for targetOverride true

--- src/tests/utils/monsterWithRandomness.test.ts
+++ src/tests/utils/monsterWithRandomness.test.ts
@@ -38,4 +38,43 @@ describe("monsterWithRandomness", () => {
       expect(newMonster).toEqual(expectedNewMonster);
     });
   });
+
+  describe("monsterRandomness with true targetOverride", () => {
+    const monsterRandomness: MonsterRandomness = {
+      dx: 1,
+      dy: -2,
+      targetOverride: true,
+    };
+
+    test("puts randomness into monster with null target", () => {
+      const monster: Monster = { x: 10, y: 10, health: 100, target: null };
+      const newMonster: Monster = monsterWithRandomness({
+        monster,
+        monsterRandomness,
+      });
+      const expectedNewMonster: Monster = {
+        ...monster,
+        target: { x: 11, y: 8 },
+      };
+      expect(newMonster).toEqual(expectedNewMonster);
+    });
+
+    test("puts randomness into monster that already has a target", () => {
+      const monster: Monster = {
+        x: 10,
+        y: 10,
+        health: 100,
+        target: { x: 8, y: 12 },
+      };
+      const newMonster: Monster = monsterWithRandomness({
+        monster,
+        monsterRandomness,
+      });
+      const expectedNewMonster: Monster = {
+        ...monster,
+        target: { x: 11, y: 8 },
+      };
+      expect(newMonster).toEqual(expectedNewMonster);
+    });
+  });
 });
