72f8928b3eb3a3defeec2ea21ae6665cf76393c4
Bryan Jennings <bryanjenningz@gmail.com>
2024-07-29 19:35:21 -0700

    Add failing test for doing nothing with monster that already has a target

--- src/tests/utils/monsterWithRandomness.test.ts
+++ src/tests/utils/monsterWithRandomness.test.ts
@@ -17,4 +17,24 @@ describe("monsterWithRandomness", () => {
     const expectedNewMonster: Monster = { ...monster, target: { x: 11, y: 8 } };
     expect(newMonster).toEqual(expectedNewMonster);
   });
+
+  test("does nothing to monster that already has a target", () => {
+    const monster: Monster = {
+      x: 10,
+      y: 10,
+      health: 100,
+      target: { x: 8, y: 12 },
+    };
+    const monsterRandomness: MonsterRandomness = {
+      dx: 1,
+      dy: -2,
+      targetOverride: false,
+    };
+    const newMonster: Monster = monsterWithRandomness({
+      monster,
+      monsterRandomness,
+    });
+    const expectedNewMonster: Monster = monster;
+    expect(newMonster).toEqual(expectedNewMonster);
+  });
 });
