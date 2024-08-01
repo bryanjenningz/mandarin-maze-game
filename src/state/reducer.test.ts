385072b2525fb6acfd0516ff7f8dcd3832895fe4
Bryan Jennings <bryanjenningz@gmail.com>
2024-07-31 19:44:47 -0700

    Add "a" to initial keysDown in test

--- src/state/reducer.test.ts
+++ src/state/reducer.test.ts
@@ -12,10 +12,10 @@ describe("reducer", () => {
     });
 
     it("adds lowercase 'arrowup' key down to state when you press 'ArrowUp'", () => {
-      const state: State = initState;
+      const state: State = { ...initState, keysDown: new Set(["a"]) };
       const action: Action = { type: "KEY_DOWN", key: "ArrowUp" };
       const newState: State = reducer(state, action);
-      const expected: State = { ...state, keysDown: new Set(["arrowup"]) };
+      const expected: State = { ...state, keysDown: new Set(["a", "arrowup"]) };
       expect(newState).toEqual(expected);
     });
   });
