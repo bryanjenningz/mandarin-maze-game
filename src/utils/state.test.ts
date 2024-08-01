9f312232b37fb9043432e284636e9d610a74b1ec
Bryan Jennings <bryanjenningz@gmail.com>
2024-07-31 19:11:00 -0700

    Add reducer test

---
+++ src/utils/state.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, test } from "vitest";
+import { initState, reducer, type State } from "./state";
+import type { Action, Bullet, Monster } from "../components/game";
+
+describe("reducer", () => {
+  test("bullets lower monster health", () => {
+    const monster: Monster = { x: 20, y: 20, health: 100, target: null };
+    const bullet: Bullet = { x: 22, y: 20, dx: -2, dy: 0 };
+    const state: State = {
+      ...initState,
+      monsters: [monster],
+      bullets: [bullet],
+    };
+    const action: Action = {
+      type: "TICK",
+      time: 10_000,
+      monsterRandomness: [{ dx: -1, dy: -1, override: false }],
+    };
+    const newState: State = reducer(state, action);
+    const expectedNewState: State = {
+      ...state,
+      monsters: [{ ...monster, health: 90, y: 19, target: { x: 19, y: 19 } }],
+      bullets: [],
+    };
+    expect(newState).toEqual(expectedNewState);
+  });
+});
