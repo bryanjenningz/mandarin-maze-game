097b8779729d80888c8a632c9763d6fc98c2b5ee
Bryan Jennings <bryanjenningz@gmail.com>
2024-07-31 19:27:24 -0700

    Start over

--- src/utils/utilities.ts
+++
@@ -1,9 +0,0 @@
-export const clamp = (lower: number, value: number, upper: number): number => {
-  return Math.max(lower, Math.min(upper, value));
-};
-
-export const range = (lower: number, upper: number): number[] => {
-  const result: number[] = [];
-  for (let i = lower; i < upper; i += 1) result.push(i);
-  return result;
-};
