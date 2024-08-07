3599dd3087dd8fd61ef5552188970e793c48095f
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-06 19:00:08 -0700

    Add gray screen

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,7 +1,7 @@
 export const Game = (): JSX.Element => {
   return (
-    <div className="text-white bg-black">
-      <h1 className="text-center">2D Maze Game</h1>
+    <div className="text-white bg-black w-full h-[100svh] flex justify-center items-center">
+      <div className="aspect-square w-full max-w-2xl bg-gray-800"></div>
     </div>
   );
 };
