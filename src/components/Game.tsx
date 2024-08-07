c35d163037da9dd64843a33f50a0b0d21bce286a
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-06 19:58:41 -0700

    Show monster health

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -62,7 +62,16 @@ export const Game = (): JSX.Element => {
                 width: `${(BLOCK_SIZE / SCREEN_SIZE) * 100}%`,
                 height: `${(BLOCK_SIZE / SCREEN_SIZE) * 100}%`,
               }}
-            ></div>
+            >
+              {monster.health > 0 && (
+                <div className="absolute -top-4 left-0 right-0 h-2 bg-red-700">
+                  <div
+                    className="absolute top-0 left-0 bottom-0 bg-green-500"
+                    style={{ width: `${monster.health}%` }}
+                  ></div>
+                </div>
+              )}
+            </div>
           );
         })}
 
