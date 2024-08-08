e1956ba93bd4074e7ec79c3110bd8c49162dfe94
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-07 21:07:18 -0700

    Make bullets slower

--- src/state/constants.ts
+++ src/state/constants.ts
@@ -2,5 +2,5 @@ export const BLOCK_SIZE = 20;
 export const BULLET_SIZE = 4;
 export const BULLET_DAMAGE = 10;
 export const BULLET_FIRE_DELAY = 200;
-export const BULLET_SPEED = 4;
+export const BULLET_SPEED = 2;
 export const SCREEN_SIZE = 400;
