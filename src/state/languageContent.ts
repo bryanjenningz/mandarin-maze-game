f6e94dad4adb14844d6e1fb7e396093d8cb5a0a1
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-18 14:12:24 -0700

    Change structure of language content

--- src/state/languageContent.ts
+++ src/state/languageContent.ts
@@ -9,9 +9,9 @@ export type Vocab = {
   sentenceMeaning: string;
 };
 
-export const languageVocab: Record<Language, Vocab[]> = {
-  MANDARIN: [
-    {
+export const languageVocab: Record<Language, Vocab>[] = [
+  {
+    MANDARIN: {
       word: "來",
       wordPronunciation: "lai2",
       wordMeaning: "come",
@@ -19,41 +19,7 @@ export const languageVocab: Record<Language, Vocab[]> = {
       sentencePronunciation: "lai2 yi2 xia4",
       sentenceMeaning: "Come over here!",
     },
-    {
-      word: "等",
-      wordPronunciation: "deng3",
-      wordMeaning: "wait",
-      sentence: "等一下！",
-      sentencePronunciation: "deng3 yi2 xia4",
-      sentenceMeaning: "Wait a second!",
-    },
-    {
-      word: "快",
-      wordPronunciation: "kuai4",
-      wordMeaning: "fast",
-      sentence: "快一點！",
-      sentencePronunciation: "kuai4 yi4 dian3",
-      sentenceMeaning: "Hurry up!",
-    },
-    {
-      word: "哪",
-      wordPronunciation: "na3",
-      wordMeaning: "which",
-      sentence: "哪一個？",
-      sentencePronunciation: "na3 yi2 ge4",
-      sentenceMeaning: "Which one?",
-    },
-    {
-      word: "试",
-      wordPronunciation: "shi4",
-      wordMeaning: "to try",
-      sentence: "试一试！",
-      sentencePronunciation: "shi4 yi2 shi4",
-      sentenceMeaning: "Try it!",
-    },
-  ],
-  SPANISH: [
-    {
+    SPANISH: {
       word: "venir",
       wordPronunciation: "",
       wordMeaning: "come",
@@ -61,7 +27,18 @@ export const languageVocab: Record<Language, Vocab[]> = {
       sentencePronunciation: "",
       sentenceMeaning: "Come over here!",
     },
-    {
+  },
+
+  {
+    MANDARIN: {
+      word: "等",
+      wordPronunciation: "deng3",
+      wordMeaning: "wait",
+      sentence: "等一下！",
+      sentencePronunciation: "deng3 yi2 xia4",
+      sentenceMeaning: "Wait a second!",
+    },
+    SPANISH: {
       word: "esperar",
       wordPronunciation: "",
       wordMeaning: "to wait",
@@ -69,7 +46,18 @@ export const languageVocab: Record<Language, Vocab[]> = {
       sentencePronunciation: "",
       sentenceMeaning: "Wait a moment!",
     },
-    {
+  },
+
+  {
+    MANDARIN: {
+      word: "快",
+      wordPronunciation: "kuai4",
+      wordMeaning: "fast",
+      sentence: "快一點！",
+      sentencePronunciation: "kuai4 yi4 dian3",
+      sentenceMeaning: "Hurry up!",
+    },
+    SPANISH: {
       word: "rápido",
       wordPronunciation: "",
       wordMeaning: "fast",
@@ -77,7 +65,18 @@ export const languageVocab: Record<Language, Vocab[]> = {
       sentencePronunciation: "",
       sentenceMeaning: "Go faster!",
     },
-    {
+  },
+
+  {
+    MANDARIN: {
+      word: "哪",
+      wordPronunciation: "na3",
+      wordMeaning: "which",
+      sentence: "哪一個？",
+      sentencePronunciation: "na3 yi2 ge4",
+      sentenceMeaning: "Which one?",
+    },
+    SPANISH: {
       word: "cuál",
       wordPronunciation: "",
       wordMeaning: "which",
@@ -85,7 +84,18 @@ export const languageVocab: Record<Language, Vocab[]> = {
       sentencePronunciation: "",
       sentenceMeaning: "Which one is good?",
     },
-    {
+  },
+
+  {
+    MANDARIN: {
+      word: "试",
+      wordPronunciation: "shi4",
+      wordMeaning: "to try",
+      sentence: "试一试！",
+      sentencePronunciation: "shi4 yi2 shi4",
+      sentenceMeaning: "Try it!",
+    },
+    SPANISH: {
       word: "intentar",
       wordPronunciation: "",
       wordMeaning: "to try to do something",
@@ -93,5 +103,5 @@ export const languageVocab: Record<Language, Vocab[]> = {
       sentencePronunciation: "",
       sentenceMeaning: "I'll try to do it",
     },
-  ],
-};
+  },
+];
