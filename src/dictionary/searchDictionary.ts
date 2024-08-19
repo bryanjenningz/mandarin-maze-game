e337fd91fc2183acc8560a41c1c1a938f6998d9d
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-18 18:07:45 -0700

    Add searchDictionary

--- src/dictionary/searchDictionary.ts
+++ src/dictionary/searchDictionary.ts
@@ -1,8 +1,81 @@
+import { parseLine } from "./parseDictionary";
 import type { Dictionary, DictionaryEntry } from "./types";
+
+const MAX_ENTRIES = 10;
 
 export const searchDictionary = (
   dictionary: Dictionary,
   word: string
 ): DictionaryEntry[] => {
-  return [];
+  const entries: DictionaryEntry[] = [];
+  while (word.length > 0) {
+    let result = binarySearch(
+      dictionary.traditional,
+      word,
+      (line) => parseLine(line)?.traditional ?? null
+    );
+    if (result) {
+      return dictionary.traditional
+        .slice(result.start, result.end + 1)
+        .map(parseLine)
+        .filter(Boolean)
+        .slice(0, MAX_ENTRIES);
+    }
+    result = binarySearch(
+      dictionary.simplified,
+      word,
+      (line) => parseLine(line)?.simplified ?? null
+    );
+    if (result) {
+      return dictionary.simplified
+        .slice(result.start, result.end + 1)
+        .map(parseLine)
+        .filter(Boolean)
+        .slice(0, MAX_ENTRIES);
+    }
+    word = word.slice(0, -1);
+  }
+  return entries;
+};
+
+const binarySearch = (
+  lines: string[],
+  target: string,
+  transformLine: (line: string) => string | null
+): { start: number; end: number } | null => {
+  let low = 0;
+  let high = lines.length - 1;
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const entry = lines[mid] ? transformLine(lines[mid]) : null;
+    if (!entry) {
+      console.warn(`Invalid line at index ${mid}: ${lines[mid]}`);
+      return null;
+    }
+    if (entry === target) {
+      let start = mid;
+      let startLine = lines[start];
+      while (start > 0 && startLine && transformLine(startLine) === entry) {
+        start -= 1;
+        startLine = lines[start];
+      }
+      let end = mid;
+      let endLine = lines[end];
+      while (
+        end < lines.length - 1 &&
+        endLine &&
+        transformLine(endLine) === entry
+      ) {
+        end += 1;
+        endLine = lines[end];
+      }
+      return { start, end };
+    }
+    if (entry < target) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return null;
 };
