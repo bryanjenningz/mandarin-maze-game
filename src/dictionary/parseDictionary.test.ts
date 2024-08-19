a307373facea9e6a9d9178648b2daeda5c1ae3d0
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-18 17:14:21 -0700

    Add parseDictionary

--- src/dictionary/parseDictionary.test.ts
+++ src/dictionary/parseDictionary.test.ts
@@ -1,6 +1,25 @@
 import { describe, expect, it } from "vitest";
-import type { DictionaryEntry } from "./types";
-import { parseLine } from "./parseDictionary";
+import type { Dictionary, DictionaryEntry } from "./types";
+import { parseDictionary, parseLine } from "./parseDictionary";
+
+describe("parseDictionary", () => {
+  it("turns dictionary text into a dictionary", () => {
+    const dictionaryText = `你們 你们 [nǐ men] /you (plural)/
+你好 你好 [nǐ hǎo] /hello/hi/`;
+    const dictionary = parseDictionary(dictionaryText);
+    const expected: Dictionary = {
+      traditional: [
+        "你們 你们 [nǐ men] /you (plural)/",
+        "你好 你好 [nǐ hǎo] /hello/hi/",
+      ],
+      simplified: [
+        "你們 你们 [nǐ men] /you (plural)/",
+        "你好 你好 [nǐ hǎo] /hello/hi/",
+      ],
+    };
+    expect(dictionary).toEqual(expected);
+  });
+});
 
 describe("parseLine", () => {
   it("turns a dictionary line into a dictionary entry", () => {
--- src/dictionary/parseDictionary.ts
+++ src/dictionary/parseDictionary.ts
@@ -1,4 +1,25 @@
-import type { DictionaryEntry } from "./types";
+import type { Dictionary, DictionaryEntry } from "./types";
+
+export const parseDictionary = (dictionaryText: string): Dictionary => {
+  const lines = dictionaryText.split("\n");
+  const traditional = lines.sort((a, b) => {
+    const parsedA = parseLine(a);
+    const parsedB = parseLine(b);
+    if (!parsedA || !parsedB) return 0;
+    if (parsedA.traditional < parsedB.traditional) return -1;
+    if (parsedA.traditional > parsedB.traditional) return 1;
+    return 0;
+  });
+  const simplified = lines.slice().sort((a, b) => {
+    const parsedA = parseLine(a);
+    const parsedB = parseLine(b);
+    if (!parsedA || !parsedB) return 0;
+    if (parsedA.simplified < parsedB.simplified) return -1;
+    if (parsedA.simplified > parsedB.simplified) return 1;
+    return 0;
+  });
+  return { traditional, simplified };
+};
 
 const LINE_PATTERN =
   /^(?<traditional>[^ ]+) (?<simplified>[^ ]+) \[(?<pronunciation>[^\]]+)\] \/(?<meaning>.+)\/$/;
