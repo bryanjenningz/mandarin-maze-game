d7e0da60baaa969af177b27ac0f4eafed8331688
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-18 17:02:12 -0700

    Add DictionaryEntry type

--- src/dictionary/types.ts
+++ src/dictionary/types.ts
@@ -2,3 +2,10 @@ export type Dictionary = {
   traditional: string[];
   simplified: string[];
 };
+
+export type DictionaryEntry = {
+  traditional: string;
+  simplified: string;
+  pronunciation: string;
+  meaning: string;
+};
