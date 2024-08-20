ef24def7bd5935ac0e40466a52e01645e6e1b55d
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-20 03:31:42 -0700

    Test closestMonster returns null when there are no monsters

---
+++ src/state/utils.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+import { closestMonster } from "./utils";
+import type { Monster, Player } from "./types";
+
+describe("closestMonster", () => {
+  it("returns the null if there are no monsters", () => {
+    const player: Player = { x: 0, y: 0, size: 20, health: 100 };
+    const monsters: Monster[] = [];
+    const closest = closestMonster(player, monsters);
+    const expected = null;
+    expect(closest).toEqual(expected);
+  });
+});
