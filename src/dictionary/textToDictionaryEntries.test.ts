18c6c63726ae05ee83909ba4c167854250ab08b8
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-19 18:30:54 -0700

    Add loadDictionary for tests

--- src/dictionary/textToDictionaryEntries.test.ts
+++ src/dictionary/textToDictionaryEntries.test.ts
@@ -1,11 +1,23 @@
-import { describe, expect, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 import { textToDictionaryEntries } from "./textToDictionaryEntries";
-import type { DictionaryEntry } from "./types";
+import type { Dictionary, DictionaryEntry } from "./types";
 import { parseDictionary } from "./parseDictionary";
+import * as fs from "fs/promises";
+
+const loadDictionary = async (): Promise<Dictionary> => {
+  const buffer = await fs.readFile("./public/dictionary.txt");
+  const text = buffer.toString();
+  return parseDictionary(text);
+};
+
+let dictionary: Dictionary;
 
 describe("textToDictionaryEntries", () => {
+  beforeAll(async () => {
+    dictionary = await loadDictionary();
+  });
+
   it("converts empty text to an empty array", () => {
-    const dictionary = parseDictionary("");
     const text = "";
     const dictionaryEntries = textToDictionaryEntries(dictionary, text);
     const expected: DictionaryEntry[] = [];
@@ -13,7 +25,6 @@ describe("textToDictionaryEntries", () => {
   });
 
   it("converts text to a dictionary entries", () => {
-    const dictionary = parseDictionary(``);
     const text = "反應又快又邏輯明確";
     const dictionaryEntries = textToDictionaryEntries(dictionary, text);
     const expected: DictionaryEntry[] = [];
