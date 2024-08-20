e77e7b437048598c9f585d32ad7e059e037979a1
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-19 18:40:32 -0700

    Implement textToDictionaryEntries

--- src/dictionary/textToDictionaryEntries.test.ts
+++ src/dictionary/textToDictionaryEntries.test.ts
@@ -27,7 +27,42 @@ describe("textToDictionaryEntries", () => {
   it("converts text to a dictionary entries", () => {
     const text = "反應又快又邏輯明確";
     const dictionaryEntries = textToDictionaryEntries(dictionary, text);
-    const expected: DictionaryEntry[] = [];
+    const expected: DictionaryEntry[] = [
+      {
+        meaning:
+          "to react, to respond, reaction, response, reply, chemical reaction, CL:個|个[gè]",
+        pronunciation: "fǎn yìng",
+        simplified: "反应",
+        traditional: "反應",
+      },
+      {
+        meaning:
+          "(once) again, also, both... and..., and yet, (used for emphasis) anyway",
+        pronunciation: "yòu",
+        simplified: "又",
+        traditional: "又",
+      },
+      {
+        meaning:
+          "rapid, quick, speed, rate, soon, almost, to make haste, clever, sharp (of knives or wits), forthright, plainspoken, gratified, pleased, pleasant",
+        pronunciation: "kuài",
+        simplified: "快",
+        traditional: "快",
+      },
+      {
+        meaning: "logic (loanword)",
+        pronunciation: "luó ji",
+        simplified: "逻辑",
+        traditional: "邏輯",
+      },
+      {
+        meaning:
+          "clear-cut; definite; explicit, to clarify; to specify; to make definite",
+        pronunciation: "míng qùe",
+        simplified: "明确",
+        traditional: "明確",
+      },
+    ];
     expect(dictionaryEntries).toEqual(expected);
   });
 });
--- src/dictionary/textToDictionaryEntries.ts
+++ src/dictionary/textToDictionaryEntries.ts
@@ -1,9 +1,26 @@
+import { searchDictionary } from "./searchDictionary";
 import type { Dictionary, DictionaryEntry } from "./types";
+
+const MAX_WORD_LENGTH = 12;
 
 export const textToDictionaryEntries = (
   dictionary: Dictionary,
   text: string
 ): DictionaryEntry[] => {
   const dictionaryEntries: DictionaryEntry[] = [];
+  const seenWords = new Set<string>();
+  let i = 0;
+  while (i < text.length) {
+    const results = searchDictionary(
+      dictionary,
+      text.slice(i, i + MAX_WORD_LENGTH)
+    );
+    const word = results[0]?.traditional;
+    if (word && !seenWords.has(word)) {
+      seenWords.add(word);
+      dictionaryEntries.push(...results);
+    }
+    i += word?.length ?? 1;
+  }
   return dictionaryEntries;
 };
