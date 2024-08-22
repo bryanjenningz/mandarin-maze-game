e822d5ce37ead03afaba54d8c40450beb5a44411
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-22 07:55:10 -0700

    Test playerFromGameMap returns player

--- src/state/gameMap.test.ts
+++ src/state/gameMap.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
-import { monstersFromGameMap } from "./gameMap";
-import type { GameMap, Monster } from "./types";
+import { monstersFromGameMap, playerFromGameMap } from "./gameMap";
+import type { GameMap, Monster, Player } from "./types";
 
 describe("gameMap", () => {
   describe("monstersFromGameMap", () => {
@@ -21,6 +21,22 @@ describe("gameMap", () => {
         { health: 100, size: 20, target: null, x: 40, y: 60 },
       ];
       expect(monsters).toEqual(expected);
+    });
+  });
+
+  describe("playerFromGameMap", () => {
+    it("returns player in the game map", () => {
+      const gameMap: GameMap = [
+        "   P",
+        "M M ",
+        "    ",
+        " MM ",
+        "    ",
+        "    ",
+      ].map((x) => x.split(""));
+      const player: Player | null = playerFromGameMap(gameMap);
+      const expected: Player = { x: 60, y: 0, size: 20, health: 100 };
+      expect(player).toEqual(expected);
     });
   });
 });
