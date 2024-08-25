707b7f9513e8ca375dcd148199869037875f728b
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-25 16:04:52 -0700

    Move NewWord component to new file

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -8,9 +8,8 @@ import { WallBlock } from "./WallBlock";
 import { loadDictionary } from "../dictionary/loadDictionary";
 import { parseDictionary } from "../dictionary/parseDictionary";
 import { GameStart } from "./GameStart";
-import { classNames } from "./utils";
 import { generateMonsterMoves } from "../state/random";
-import type { MandarinWord } from "../state/types";
+import { NewWord } from "./NewWord";
 
 export const Game = (): JSX.Element => {
   const [state, dispatch] = useReducer(reducer, initState);
@@ -94,52 +93,6 @@ export const Game = (): JSX.Element => {
           />
         )}
       </div>
-    </div>
-  );
-};
-
-type NewWordProps = {
-  mandarinWord: MandarinWord;
-  resumeGame: () => void;
-};
-
-const NewWord = ({ mandarinWord, resumeGame }: NewWordProps): JSX.Element => {
-  return (
-    <div className="relative bg-gray-800 text-white p-4 max-w-2xl w-full flex flex-col items-center justify-center gap-4 z-20">
-      <h2 className="text-xl">You found a new word!</h2>
-      <div className="flex gap-4">
-        <div className="text-lg font-bold">{mandarinWord.word}</div>
-        <div className="text-lg">{mandarinWord.pronunciation}</div>
-      </div>
-      <div className="text-gray-200">{mandarinWord.meaning}</div>
-      <div className="text-gray-200">
-        {((): JSX.Element => {
-          const word = mandarinWord.word;
-          const wordIndex = mandarinWord.context.indexOf(word);
-          return (
-            <>
-              {mandarinWord.context.split("").map((ch, i) => {
-                const highlighted =
-                  i >= wordIndex && i < wordIndex + word.length;
-                return (
-                  <span
-                    key={`${ch}-${i}`}
-                    className={classNames(highlighted && "bg-blue-400")}
-                  >
-                    {ch}
-                  </span>
-                );
-              })}
-            </>
-          );
-        })()}
-      </div>
-      <button
-        className="py-2 px-4 rounded-lg bg-blue-800 text-white hover:brightness-110 transition duration-300 text-lg"
-        onClick={resumeGame}
-      >
-        Got it!
-      </button>
     </div>
   );
 };
---
+++ src/components/NewWord.tsx
@@ -0,0 +1,51 @@
+import type { MandarinWord } from "../state/types";
+import { classNames } from "./utils";
+
+type NewWordProps = {
+  mandarinWord: MandarinWord;
+  resumeGame: () => void;
+};
+
+export const NewWord = ({
+  mandarinWord,
+  resumeGame,
+}: NewWordProps): JSX.Element => {
+  return (
+    <div className="relative bg-gray-800 text-white p-4 max-w-2xl w-full flex flex-col items-center justify-center gap-4 z-20">
+      <h2 className="text-xl">You found a new word!</h2>
+      <div className="flex gap-4">
+        <div className="text-lg font-bold">{mandarinWord.word}</div>
+        <div className="text-lg">{mandarinWord.pronunciation}</div>
+      </div>
+      <div className="text-gray-200">{mandarinWord.meaning}</div>
+      <div className="text-gray-200">
+        {((): JSX.Element => {
+          const word = mandarinWord.word;
+          const wordIndex = mandarinWord.context.indexOf(word);
+          return (
+            <>
+              {mandarinWord.context.split("").map((ch, i) => {
+                const highlighted =
+                  i >= wordIndex && i < wordIndex + word.length;
+                return (
+                  <span
+                    key={`${ch}-${i}`}
+                    className={classNames(highlighted && "bg-blue-400")}
+                  >
+                    {ch}
+                  </span>
+                );
+              })}
+            </>
+          );
+        })()}
+      </div>
+      <button
+        className="py-2 px-4 rounded-lg bg-blue-800 text-white hover:brightness-110 transition duration-300 text-lg"
+        onClick={resumeGame}
+      >
+        Got it!
+      </button>
+    </div>
+  );
+};
