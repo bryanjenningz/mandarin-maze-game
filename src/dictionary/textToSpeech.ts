66a1b0a9ac23be777f7306a95077cd6c2a4600fe
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-25 18:00:12 -0700

    Add textToSpeech function

---
+++ src/dictionary/textToSpeech.ts
@@ -0,0 +1,7 @@
+export const textToSpeech = (text: string) => {
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.lang = "zh-CN";
+  utterance.rate = 0.85;
+  speechSynthesis.cancel();
+  speechSynthesis.speak(utterance);
+};
