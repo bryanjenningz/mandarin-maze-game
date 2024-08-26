0a38df638ef59a6c91959af23c26c0203c24b635
Bryan Jennings <bryanjenningz@gmail.com>
2024-08-25 18:18:16 -0700

    Reduce context words in NewWord component

--- src/components/NewWord.tsx
+++ src/components/NewWord.tsx
@@ -30,20 +30,29 @@ export const NewWord = ({
         {((): JSX.Element => {
           const word = mandarinWord.word;
           const wordIndex = mandarinWord.context.indexOf(word);
+          const CONTEXT_PADDING = 8;
+          const contextSlice = mandarinWord.context.slice(
+            Math.max(0, wordIndex - CONTEXT_PADDING),
+            Math.max(0, wordIndex + word.length + CONTEXT_PADDING)
+          );
+          const wordIndexContextSlice = contextSlice.indexOf(word);
           return (
             <>
-              {mandarinWord.context.split("").map((ch, i) => {
+              {"..."}
+              {contextSlice.split("").map((ch, i) => {
                 const highlighted =
-                  i >= wordIndex && i < wordIndex + word.length;
+                  i >= wordIndexContextSlice &&
+                  i < wordIndexContextSlice + word.length;
                 return (
                   <span
                     key={`${ch}-${i}`}
-                    className={classNames(highlighted && "bg-blue-400")}
+                    className={classNames(highlighted && "bg-blue-800")}
                   >
                     {ch}
                   </span>
                 );
               })}
+              {"..."}
             </>
           );
         })()}
